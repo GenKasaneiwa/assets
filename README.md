7d1a6f8548aacfb79159ab8c1523eb62f5fede37
GenKasaneiwa <gen@gnw.jp>
2021-11-25 23:02:53 +0900

    test: add

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# assets
+# Assets
+
+## ヘルパーモジュール
+
+配信用 CDN
+```
+<script src="https://cdn.jsdelivr.net/gh/GenKasaneiwa/library/index.js"></script>
+```
--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-alert("hoge");
+//----------------------------------------------------------------------
+//   共通コンポーネント
+//----------------------------------------------------------------------
+
+//  display module
+//---------------------------------------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+   const container = document.createElement('div');
+   container.className = 'console-container';
+   container.style.cssText = 'position:fixed;right:40px;bottom:20px;z-index:9999;width:150px;';
+   document.body.appendChild(container);
+});
+
+function display(val){
+   console.log(val);
+
+   window.addEventListener('load', () => {
+      let div_el = document.createElement('div');
+      let target_container = document.querySelector('.console-container');
+      div_el.innerText = val;
+      div_el.style.cssText = 'position:relative;right:0;bottom:0;margin:0 0 10px 0;filter: drop-shadow(1px 1px 4px #a9a9a9);background:#fff;border-radius:5px;padding:10px;width:100%';
+      target_container.appendChild(div_el);
+   });
+}
+
+~(_=>{
+})``;
